f660b13fb9a5e505c3e20dc42699c782f2bc46f9
zhuweiheng <554055345@qq.com>
2017-11-20 15:50:20 +0800

    更新了新闻添加相关功能

--- server/application/controllers/Api.php
+++ server/application/controllers/Api.php
@@ -1,5 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	use \QCloud_WeApp_SDK\Auth\LoginService as LoginService;
+	use QCloud_WeApp_SDK\Constants as Constants;
+
 class Api extends CI_Controller{
 
 	/**
@@ -34,8 +37,146 @@ class Api extends CI_Controller{
 		);
 		$this->json($data);
 
-		$this->load->view('api',$data);
+	}
+
+	public  function create(){
+		//检查用户状态
+		$result = LoginService::check();
+
+        if ($result['loginState'] === Constants::S_AUTH) {
+        	$data = $this->input->post(NULL,TRUE);//获取全部数据并XXS过滤
+        	$data = json_decode($data);
+
+        	//预定义插入数据库的数组
+			$create = array(
+				'uid' => $data['uid'],
+				'title' => $data['title'],
+				'content' => $data['content'],
+				'create_time' => time(),
+				'img_url' => $data['img_url']
+			);
+			$this->load->database();
+			$this->db->insert('cUserNews',$create);    //参数1表面  参数2数据
+			$result = $this->db->affected_rows(); //影响行数
+			if ($result){
+				$this->json([
+	                'code' => 0,
+	                'data' => array(
+	                	'successmsg' => '添加成功'
+	                ),
+	                'statusCode' => 200
+            	]);
+			} else{
+				$this->json([
+	                'code' => -1,
+	                'data' => array(
+	                	'errormsg' => '添加失败'
+	                ),
+	                'statusCode' => 100
+	            ]);
+			}
+        } else {
+            $this->json([
+                'code' => -1,
+                'data' => array(
+                	'errormsg' => '未登录'
+                ),
+                'statusCode' => 100
+            ]);
+        }
+
+
 
 	}
-	
+
+	public function save(){
+		//检查用户状态
+		$result = LoginService::check();
+
+        if ($result['loginState'] === Constants::S_AUTH) {
+        	$data = $this->input->post(NULL,TRUE);//获取全部数据并XXS过滤
+        	$data = json_decode($data);
+
+        	//预定义插入数据库的数组
+			$create = array(
+				'uid' => $data['uid'],
+				'title' => $data['title'],
+				'content' => $data['content'],
+				'create_time' => time(),
+				'img_url' => $data['img_url']
+			);
+			$this->load->database();
+			$this->db->replace('cUserNews',$create);    //参数1表面  参数2数据
+			$result = $this->db->affected_rows(); //影响行数
+			if ($result){
+				$this->json([
+	                'code' => 0,
+	                'data' => array(
+	                	'successmsg' => '修改成功'
+	                ),
+	                'statusCode' => 200
+            	]);
+			} else{
+				$this->json([
+	                'code' => -1,
+	                'data' => array(
+	                	'errormsg' => '修改失败'
+	                ),
+	                'statusCode' => 100
+	            ]);
+			}
+        } else {
+            $this->json([
+                'code' => -1,
+                'data' => array(
+                	'errormsg' => '未登录'
+                ),
+                'statusCode' => 100
+            ]);
+        }
+	}
+
+	public function del(){
+  		//检查用户状态
+		$result = LoginService::check();
+
+        if ($result['loginState'] === Constants::S_AUTH) {
+        	$data = $this->input->post(NULL,TRUE);//获取全部数据并XXS过滤
+        	$data = json_decode($data);
+
+			$this->load->database();
+			$this->db->delete('cUserNews',array('new_id' => $data['new_id']));
+			$result = $this->db->affected_rows(); //影响行数
+			if ($result){
+				$this->json([
+	                'code' => 0,
+	                'data' => array(
+	                	'successmsg' => '删除成功'
+	                ),
+	                'statusCode' => 200
+            	]);
+			} else{
+				$this->json([
+	                'code' => -1,
+	                'data' => array(
+	                	'errormsg' => '删除失败'
+	                ),
+	                'statusCode' => 100
+	            ]);
+			}
+        } else {
+            $this->json([
+                'code' => -1,
+                'data' => array(
+                	'errormsg' => '未登录'
+                ),
+                'statusCode' => 100
+            ]);
+        }
+	}
+
+	//用户收藏接口
+	public function collect(){
+		
+	}	
 }
